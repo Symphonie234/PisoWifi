8a3cf2989c834881fb536663ed3825452b6c073f
Gerand <gerandparawan23@gmail.com>
2025-12-03 23:14:02 +0800

    feat(portal): convert portal views to Tailwind utility classes

--- stubs/resources/views/portal/landing.blade.php
+++ stubs/resources/views/portal/landing.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>PISO-WIFI Portal</title>
-    <link rel="stylesheet" href="/css/portal.css">
+    @vite(['resources/js/app.js'])
 </head>
 <body class="page">
     <main class="card">
